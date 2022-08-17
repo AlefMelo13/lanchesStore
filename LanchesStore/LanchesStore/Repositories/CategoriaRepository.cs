fe4df4301c4a627762f35d675a2e57b8e6d8f5dd
AlefMelo <alefmelo13@gmail.com>
2022-08-17 10:51:16 -0300

    atualização projeto

--- LanchesStore/LanchesStore/Repositories/CategoriaRepository.cs
+++ LanchesStore/LanchesStore/Repositories/CategoriaRepository.cs
@@ -1,10 +1,18 @@
-﻿using LanchesStore.Models;
+﻿using LanchesStore.Context;
+using LanchesStore.Models;
 using LanchesStore.Repositories.Interfaces;
 
 namespace LanchesStore.Repositories
 {
     public class CategoriaRepository : ICategoriaRepository
     {
-        public IEnumerable<Categoria> Categorias => throw new NotImplementedException();
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
     }
 }
